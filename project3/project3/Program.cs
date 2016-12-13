c344436bfbdeec8aadde0a07411e699f034b15fb
xavier <xavier@SERRATE>
2016-12-13 16:25:49 +0100

    benvinguda

---
+++ project3/project3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mon");
+            Console.WriteLine("Hola uop");
+            Console.WriteLine("Hola sgv");
+        }
+    }
+}
